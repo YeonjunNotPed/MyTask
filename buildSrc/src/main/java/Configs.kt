6d8fa3166287b81908dcda09034995c5e6ceac43
yeonjun <kgbduswns@gmail.com>
2024-04-11 20:00:00 +0900

    MIN_SDK 26, TARGET_SDK 34 수정

--- buildSrc/src/main/java/Configs.kt
+++ buildSrc/src/main/java/Configs.kt
@@ -3,8 +3,8 @@ object Configs {
     const val COMPILE_SDK = 34
 
     //Android Default Config
-    const val MIN_SDK = 24
-    const val TARGET_SDK = 33
+    const val MIN_SDK = 26
+    const val TARGET_SDK = 34
     const val VERSION_CODE = 4
     const val VERSION_NAME = "1.0.0"
 }
