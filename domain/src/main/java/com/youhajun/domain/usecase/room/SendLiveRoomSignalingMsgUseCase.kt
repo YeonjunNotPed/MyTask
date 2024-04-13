a286fcef703fc1d4bbafef8843964b13970d12bb
yeonjun <kgbduswns@gmail.com>
2024-04-13 15:00:00 +0900

    SendLiveRoomSignalingMsgUsecase를 NonSuspendUseCase로 변경

--- domain/src/main/java/com/youhajun/domain/usecase/room/SendLiveRoomSignalingMsgUseCase.kt
+++ domain/src/main/java/com/youhajun/domain/usecase/room/SendLiveRoomSignalingMsgUseCase.kt
@@ -3,13 +3,13 @@ package com.youhajun.domain.usecase.room
 import com.youhajun.data.models.enums.SignalingCommand
 import com.youhajun.data.repositories.RoomRepository
 import com.youhajun.domain.models.enums.SignalingType
-import com.youhajun.domain.usecase.UseCase
+import com.youhajun.domain.usecase.NonSuspendUseCase
 import javax.inject.Inject
 
 class SendLiveRoomSignalingMsgUseCase @Inject constructor(
     private val roomRepository: RoomRepository
-) : UseCase<Pair<SignalingType, String>, Unit>() {
-    override suspend fun invoke(request: Pair<SignalingType, String>) {
+) : NonSuspendUseCase<Pair<SignalingType, String>, Unit>() {
+    override fun invoke(request: Pair<SignalingType, String>) {
         roomRepository.sendSignalingMessage(SignalingCommand.typeOf(request.first.type), request.second)
     }
 }
