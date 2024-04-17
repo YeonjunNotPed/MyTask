5677f7f166935ab8aa0eb0b80b488546fae3a6d9
yeonjun <kgbduswns@gmail.com>
2024-04-17 17:00:00 +0900

    모듈 분리 -> core:datastore 모듈 생성

---
+++ core/datastore/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.jetbrains.android)
+    alias(libs.plugins.hilt)
+    kotlin("kapt")
+}
+
+android {
+    namespace = "com.youhajun.core.datastore"
+    compileSdk = Configs.COMPILE_SDK
+
+    kotlinOptions {
+        jvmTarget = Configs.JVM_TARGET
+    }
+
+    defaultConfig {
+        minSdk = Configs.MIN_SDK
+    }
+}
+
+dependencies {
+    implementation(libs.bundles.datastore)
+    implementation(libs.hilt)
+    kapt(libs.hilt.compiler)
+}
