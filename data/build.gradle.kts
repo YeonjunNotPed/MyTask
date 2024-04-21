7f4f99601ffd7b473dae44efc5602a63cf0861c9
yeonjun <kgbduswns@gmail.com>
2024-04-21 13:00:00 +0900

    data 모듈 build.gradle 업데이트

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -2,32 +2,24 @@ import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
 
 plugins {
     kotlin("kapt")
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.dagger.hilt.android")
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.jetbrains.android)
+    alias(libs.plugins.hilt)
 }
 
 android {
     namespace = "com.youhajun.data"
     compileSdk = Configs.COMPILE_SDK
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+    kotlinOptions {
+        jvmTarget = Configs.JVM_TARGET
     }
 
     defaultConfig {
         minSdk = Configs.MIN_SDK
-        buildConfigField("String", "BASE_URL", getProperty("BASE_URL"))
-        buildConfigField("String", "WEBSOCKET_BASE_URL", getProperty("WEBSOCKET_BASE_URL"))
-        buildConfigField("String", "CHAT_GPT_API_KEY", getProperty("CHAT_GPT_API_KEY"))
-        buildConfigField("String", "GEMINI_API_KEY", getProperty("GEMINI_API_KEY"))
-    }
-    buildFeatures {
-        buildConfig = true
     }
     buildTypes {
-        getByName("release") {
+        release {
             isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -40,27 +32,16 @@ android {
 }
 
 dependencies {
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+    implementation(project(":core:model-data"))
+    implementation(project(":core:room"))
+    implementation(project(":core:remote"))
+    implementation(project(":core:datastore"))
+    implementation(project(":core:common"))
 
+    implementation(libs.hilt)
+    kapt(libs.hilt.compiler)
 
-    implementation("androidx.room:room-runtime:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1")
-    kapt("androidx.room:room-compiler:2.6.1")
-
-    implementation("com.google.dagger:hilt-android:2.49")
-    kapt("com.google.dagger:hilt-android-compiler:2.49")
-
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
-    implementation("androidx.datastore:datastore-preferences-core:1.0.0")
-
-    implementation("androidx.security:security-crypto:1.0.0")
-
-    implementation("io.getstream:stream-webrtc-android:1.0.3")
-}
-
-fun getProperty(propertyKey: String): String {
-    return gradleLocalProperties(rootDir).getProperty(propertyKey)
+    implementation(libs.security.crypto)
+    implementation(libs.bundles.datastore)
+    implementation(libs.bundles.network)
 }
