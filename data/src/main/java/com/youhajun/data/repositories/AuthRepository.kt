5bb7a548b45f049d989ce182a7d190681a51593f
yeonjun <kgbduswns@gmail.com>
2024-04-21 17:00:00 +0900

    AuthRepository BaseRepository 제거 및 연속적인 데이터만 Flow 사용하도록 수정, ApiResult 적용

--- data/src/main/java/com/youhajun/data/repositories/AuthRepository.kt
+++ data/src/main/java/com/youhajun/data/repositories/AuthRepository.kt
@@ -1,75 +1,37 @@
 package com.youhajun.data.repositories
 
-import com.youhajun.data.Resource
 import com.youhajun.data.encrypt.Crypto
-import com.youhajun.data.models.dto.auth.MyTaskToken
-import com.youhajun.data.repositories.base.BaseRepository
 import com.youhajun.data.repositories.localDataSource.PrefDataStoreDataSource
 import com.youhajun.data.repositories.remoteDataSource.AuthRemoteDataSource
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emptyFlow
-import kotlinx.coroutines.flow.flatMapConcat
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.map
+import com.youhajun.model_data.login.MyTaskToken
+import com.youhajun.model_data.ApiResult
 import javax.inject.Inject
 
 class AuthRepository @Inject constructor(
     private val crypto: Crypto,
     private val prefDataStoreDataSource: PrefDataStoreDataSource,
     private val authRemoteDataSource: AuthRemoteDataSource
-) : BaseRepository() {
+) {
 
     companion object {
         private const val ACCESS_TOKEN_KEY = "ACCESS_TOKEN"
         private const val REFRESH_TOKEN_KEY = "REFRESH_TOKEN"
     }
 
-    fun getRefreshedMyTaskToken(refreshToken: String): Flow<Resource<MyTaskToken>> =
-        authRemoteDataSource.getRefreshedToken(refreshToken).map { myTaskApiConverter(it) }
+    suspend fun getRefreshedMyTaskToken(refreshToken: String): ApiResult<MyTaskToken> = authRemoteDataSource.getRefreshedToken(refreshToken)
 
-    fun getAccessTokenSync(): Resource<String> {
-        return prefDataStoreDataSource.getStringSync(ACCESS_TOKEN_KEY)
-    }
+    fun getAccessTokenSync(): String = prefDataStoreDataSource.getStringSync(ACCESS_TOKEN_KEY)
 
-    fun getRefreshTokenSync(): Flow<Resource<String>> = flow {
+    fun getRefreshTokenSync(): String {
         val token = prefDataStoreDataSource.getStringSync(REFRESH_TOKEN_KEY)
-        val result = if (token is Resource.Success) Resource.Success(crypto.decrypt(token.data))
-        else token
-        emit(result)
+        return crypto.decrypt(token)
     }
 
-    fun getRefreshToken(): Flow<Resource<String>> {
-        return prefDataStoreDataSource.getString(REFRESH_TOKEN_KEY).map {
-            when(it) {
-                is Resource.Success -> Resource.Success(crypto.decrypt(it.data))
-                is Resource.Error -> Resource.Error(it.errorVo.convert())
-                is Resource.Loading -> Resource.Loading()
-            }
-        }
-    }
-
-    fun getAccessToken(): Flow<Resource<String>> {
-        return prefDataStoreDataSource.getString(ACCESS_TOKEN_KEY).map {
-            when (it) {
-                is Resource.Success -> Resource.Success(crypto.decrypt(it.data))
-                is Resource.Error -> Resource.Error(it.errorVo.convert())
-                is Resource.Loading -> Resource.Loading()
-            }
-        }
-    }
-
-    @OptIn(ExperimentalCoroutinesApi::class)
-    fun saveMyTaskToken(myTaskToken: MyTaskToken): Flow<Resource<Unit>> {
+    suspend fun saveMyTaskToken(myTaskToken: MyTaskToken) {
         val accessTokenArray = crypto.encrypt(myTaskToken.accessToken)
         val refreshTokenArray = crypto.encrypt(myTaskToken.refreshToken)
-        return prefDataStoreDataSource.setString(ACCESS_TOKEN_KEY, accessTokenArray).flatMapConcat {
-            when (it) {
-                is Resource.Success -> prefDataStoreDataSource.setString(REFRESH_TOKEN_KEY, refreshTokenArray)
-                is Resource.Error -> flow { emit(Resource.Error(it.errorVo)) }
-                is Resource.Loading -> emptyFlow()
-            }
-        }
+        prefDataStoreDataSource.setString(ACCESS_TOKEN_KEY, accessTokenArray)
+        prefDataStoreDataSource.setString(REFRESH_TOKEN_KEY, refreshTokenArray)
     }
 }
 
