13ded1cf97f83577e6a038afb1806a667c9e58c4
yeonjun <kgbduswns@gmail.com>
2024-04-22 15:00:00 +0900

    GptRemoteDataSource Flow 제거 및, ApiResult 적용, api 에러 핸들링은 apiHandle에서 하도록

--- data/src/main/java/com/youhajun/data/repositories/remoteDataSource/GptRemoteDataSource.kt
+++ data/src/main/java/com/youhajun/data/repositories/remoteDataSource/GptRemoteDataSource.kt
@@ -1,19 +1,15 @@
 package com.youhajun.data.repositories.remoteDataSource
 
-import com.youhajun.data.models.dto.gpt.ChatGptRequest
-import com.youhajun.data.models.dto.gpt.ChatGptResponse
-import com.youhajun.data.network.safeResponseFlow
-import com.youhajun.data.services.ChatGptService
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import retrofit2.Response
+import com.youhajun.model_data.gpt.ChatGptRequest
+import com.youhajun.model_data.gpt.ChatGptResponse
+import com.youhajun.model_data.ApiResult
+import com.youhajun.remote.apiHandle
+import com.youhajun.remote.services.ChatGptService
 import javax.inject.Inject
-import javax.inject.Singleton
 
 class GptRemoteDataSource @Inject constructor(
     private val chatGptService: ChatGptService
 ) {
-    fun postChatGptPrompt(request:ChatGptRequest): Flow<Response<ChatGptResponse>> = flow {
-        emit(chatGptService.postPrompt(request))
-    }.safeResponseFlow()
+    suspend fun postChatGptPrompt(request: ChatGptRequest): ApiResult<ChatGptResponse> =
+        apiHandle { chatGptService.postPrompt(request) }
 }
