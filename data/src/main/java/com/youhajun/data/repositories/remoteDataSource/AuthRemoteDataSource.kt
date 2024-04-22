1761fd5969f11a9d648b925f6164644d19c8f93d
yeonjun <kgbduswns@gmail.com>
2024-04-22 13:00:00 +0900

    AuthRemoteDataSource Flow 제거 및, ApiResult 적용, api 에러 핸들링은 apiHandle에서 하도록

--- data/src/main/java/com/youhajun/data/repositories/remoteDataSource/AuthRemoteDataSource.kt
+++ data/src/main/java/com/youhajun/data/repositories/remoteDataSource/AuthRemoteDataSource.kt
@@ -1,19 +1,14 @@
 package com.youhajun.data.repositories.remoteDataSource
 
-import com.youhajun.data.models.dto.ApiResponse
-import com.youhajun.data.models.dto.auth.MyTaskToken
-import com.youhajun.data.network.safeResponseFlow
-import com.youhajun.data.services.AuthService
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import retrofit2.Response
+import com.youhajun.model_data.login.MyTaskToken
+import com.youhajun.model_data.ApiResult
+import com.youhajun.remote.myTaskApiHandle
+import com.youhajun.remote.services.AuthService
 import javax.inject.Inject
-import javax.inject.Singleton
 
 class AuthRemoteDataSource @Inject constructor(
     private val authService: AuthService
 ) {
-    fun getRefreshedToken(refreshToken: String): Flow<Response<ApiResponse<MyTaskToken>>> = flow {
-        emit(authService.getRefreshedToken(refreshToken))
-    }.safeResponseFlow()
+    suspend fun getRefreshedToken(refreshToken: String): ApiResult<MyTaskToken> =
+        myTaskApiHandle { authService.getRefreshedToken(refreshToken) }
 }
