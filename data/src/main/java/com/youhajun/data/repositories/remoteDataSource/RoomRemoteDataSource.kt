c53f78d1e2c54a98102089d6ed44252f1b245fe8
yeonjun <kgbduswns@gmail.com>
2024-04-23 13:00:00 +0900

    RoomRemoteDataSource 적절히 Flow 제거 suspend 사용, ApiResult에서 api 에러 핸들링

--- data/src/main/java/com/youhajun/data/repositories/remoteDataSource/RoomRemoteDataSource.kt
+++ data/src/main/java/com/youhajun/data/repositories/remoteDataSource/RoomRemoteDataSource.kt
@@ -1,19 +1,14 @@
 package com.youhajun.data.repositories.remoteDataSource
 
-import com.youhajun.data.models.dto.ApiResponse
-import com.youhajun.data.models.dto.room.RoomPreviewInfo
-import com.youhajun.data.network.safeResponseFlow
-import com.youhajun.data.services.RoomService
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import retrofit2.Response
+import com.youhajun.model_data.room.RoomPreviewInfo
+import com.youhajun.model_data.ApiResult
+import com.youhajun.remote.myTaskApiHandle
+import com.youhajun.remote.services.RoomService
 import javax.inject.Inject
-import javax.inject.Singleton
 
 class RoomRemoteDataSource @Inject constructor(
     private val roomService: RoomService,
 ) {
-    suspend fun getRoomPreviewInfo(): Flow<Response<ApiResponse<RoomPreviewInfo>>> = flow {
-        emit(roomService.getRoomPreviewInfo())
-    }.safeResponseFlow()
+    suspend fun getRoomPreviewInfo(): ApiResult<RoomPreviewInfo> =
+        myTaskApiHandle { roomService.getRoomPreviewInfo() }
 }
