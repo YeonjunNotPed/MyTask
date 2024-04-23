57724bf3c65203fb9b95e79a82f690c40a79a7c7
yeonjun <kgbduswns@gmail.com>
2024-04-23 17:00:00 +0900

    SignRemoteDataSource 적절히 Flow 제거 suspend 사용, ApiResult에서 api 에러 핸들링

--- data/src/main/java/com/youhajun/data/repositories/remoteDataSource/SignRemoteDataSource.kt
+++ data/src/main/java/com/youhajun/data/repositories/remoteDataSource/SignRemoteDataSource.kt
@@ -1,25 +1,19 @@
 package com.youhajun.data.repositories.remoteDataSource
 
-import com.youhajun.data.models.dto.ApiResponse
-import com.youhajun.data.models.dto.auth.MyTaskToken
-import com.youhajun.data.models.dto.sign.LoginRequest
-import com.youhajun.data.models.dto.sign.SocialLoginRequest
-import com.youhajun.data.network.safeResponseFlow
-import com.youhajun.data.services.SignService
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import retrofit2.Response
+import com.youhajun.model_data.login.LoginRequest
+import com.youhajun.model_data.login.MyTaskToken
+import com.youhajun.model_data.login.SocialLoginRequest
+import com.youhajun.model_data.ApiResult
+import com.youhajun.remote.myTaskApiHandle
+import com.youhajun.remote.services.SignService
 import javax.inject.Inject
-import javax.inject.Singleton
 
 class SignRemoteDataSource @Inject constructor(
     private val signService: SignService
 ) {
-    fun postLogin(loginRequest: LoginRequest): Flow<Response<ApiResponse<MyTaskToken>>> = flow {
-        emit(signService.postLogin(loginRequest))
-    }.safeResponseFlow()
+    suspend fun postLogin(loginRequest: LoginRequest): ApiResult<MyTaskToken> =
+        myTaskApiHandle { signService.postLogin(loginRequest) }
 
-    fun postSocialLogin(socialLoginRequest: SocialLoginRequest): Flow<Response<ApiResponse<MyTaskToken>>> = flow {
-        emit(signService.postSocialLogin(socialLoginRequest))
-    }.safeResponseFlow()
+    suspend fun postSocialLogin(socialLoginRequest: SocialLoginRequest): ApiResult<MyTaskToken> =
+        myTaskApiHandle { signService.postSocialLogin(socialLoginRequest) }
 }
