e66561d277e219a893b362a42fc97c3ee4ef002a
yeonjun <kgbduswns@gmail.com>
2024-04-25 13:00:00 +0900

    RoomRepository BaseRepository 제거 및 suspend, Flow 적절히 사용하도록 수정, ApiResult 적용

--- data/src/main/java/com/youhajun/data/repositories/RoomRepository.kt
+++ data/src/main/java/com/youhajun/data/repositories/RoomRepository.kt
@@ -1,21 +1,19 @@
 package com.youhajun.data.repositories
 
-import com.youhajun.data.Resource
-import com.youhajun.data.di.DefaultDispatcher
-import com.youhajun.data.models.dto.room.RoomPreviewInfo
-import com.youhajun.data.models.enums.SignalingCommand
-import com.youhajun.data.models.enums.SocketMessage
-import com.youhajun.data.models.enums.WebRTCSessionState
-import com.youhajun.data.models.sealeds.WebSocketStateDTO
-import com.youhajun.data.repositories.base.BaseRepository
+import com.youhajun.common.DefaultDispatcher
 import com.youhajun.data.repositories.remoteDataSource.RoomRemoteDataSource
 import com.youhajun.data.repositories.remoteDataSource.WebSocketDataSource
+import com.youhajun.model_data.room.RoomPreviewInfo
+import com.youhajun.model_data.types.SignalingCommandTypeDto
+import com.youhajun.model_data.types.SocketMessageTypeDto
+import com.youhajun.model_data.types.WebRTCSessionTypeDto
+import com.youhajun.model_data.types.WebSocketStateDto
+import com.youhajun.model_data.ApiResult
 import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
@@ -23,7 +21,6 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -33,17 +30,16 @@ class RoomRepository @Inject constructor(
     private val roomRemoteDataSource: RoomRemoteDataSource,
     private val webSocketDataSource: WebSocketDataSource,
     @DefaultDispatcher private val defaultDispatcher: CoroutineDispatcher,
-) : BaseRepository() {
+) {
 
     private lateinit var scope: CoroutineScope
 
-    private val _signalingCommandFlow = MutableSharedFlow<Pair<SignalingCommand, String>>()
-    val signalingCommandFlow: SharedFlow<Pair<SignalingCommand, String>> = _signalingCommandFlow.asSharedFlow()
+    private val _signalingCommandTypeFlow = MutableSharedFlow<Pair<SignalingCommandTypeDto, String>>()
+    val signalingCommandTypeFlow: SharedFlow<Pair<SignalingCommandTypeDto, String>> = _signalingCommandTypeFlow.asSharedFlow()
 
-    private val _sessionStateFlow = MutableStateFlow(WebRTCSessionState.Offline)
-    val sessionStateFlow: StateFlow<WebRTCSessionState> = _sessionStateFlow.asStateFlow()
-    suspend fun getRoomPreviewInfo(): Flow<Resource<RoomPreviewInfo>> =
-        roomRemoteDataSource.getRoomPreviewInfo().map { myTaskApiConverter(it) }
+    private val _sessionStateFlow = MutableStateFlow(WebRTCSessionTypeDto.Offline)
+    val sessionStateFlow: StateFlow<WebRTCSessionTypeDto> = _sessionStateFlow.asStateFlow()
+    suspend fun getRoomPreviewInfo(): ApiResult<RoomPreviewInfo> = roomRemoteDataSource.getRoomPreviewInfo()
 
     fun connectLiveRoom() {
         scope = CoroutineScope(SupervisorJob() + defaultDispatcher)
@@ -52,18 +48,18 @@ class RoomRepository @Inject constructor(
     }
 
     fun dispose() {
-        _sessionStateFlow.update { WebRTCSessionState.Offline }
+        _sessionStateFlow.update { WebRTCSessionTypeDto.Offline }
         scope.cancel()
         webSocketDataSource.disconnect()
     }
 
-    fun sendSignalingMessage(signalingCommand: SignalingCommand, message: String) {
-        val totalMessage = "$signalingCommand $message"
+    fun sendSignalingMessage(signalingCommandTypeDto: SignalingCommandTypeDto, message: String) {
+        val totalMessage = "$signalingCommandTypeDto $message"
         sendMessage(totalMessage)
     }
 
-    fun sendSocketMessage(socketMessage: SocketMessage, message: String) {
-        val totalMessage = "$socketMessage $message"
+    fun sendSocketMessage(socketMessageTypeDto: SocketMessageTypeDto, message: String) {
+        val totalMessage = "$socketMessageTypeDto $message"
         sendMessage(totalMessage)
     }
 
@@ -74,38 +70,38 @@ class RoomRepository @Inject constructor(
     private fun collectSocket() {
         scope.launch {
             webSocketDataSource.socketFlow
-                .filter { it is WebSocketStateDTO.Message }
+                .filter { it is WebSocketStateDto.Message }
                 .collect {
-                    if (it is WebSocketStateDTO.Message) onMessage(it.text)
+                    if (it is WebSocketStateDto.Message) onMessage(it.text)
                 }
         }
     }
 
     private suspend fun onMessage(text: String) {
         when {
-            text.startsWith(SignalingCommand.STATE.toString(), true) ->
+            text.startsWith(SignalingCommandTypeDto.STATE.toString(), true) ->
                 handleStateMessage(text)
 
-            text.startsWith(SignalingCommand.OFFER.toString(), true) ->
-                handleSignalingCommand(SignalingCommand.OFFER, text)
+            text.startsWith(SignalingCommandTypeDto.OFFER.toString(), true) ->
+                handleSignalingCommand(SignalingCommandTypeDto.OFFER, text)
 
-            text.startsWith(SignalingCommand.ANSWER.toString(), true) ->
-                handleSignalingCommand(SignalingCommand.ANSWER, text)
+            text.startsWith(SignalingCommandTypeDto.ANSWER.toString(), true) ->
+                handleSignalingCommand(SignalingCommandTypeDto.ANSWER, text)
 
-            text.startsWith(SignalingCommand.ICE.toString(), true) ->
-                handleSignalingCommand(SignalingCommand.ICE, text)
+            text.startsWith(SignalingCommandTypeDto.ICE.toString(), true) ->
+                handleSignalingCommand(SignalingCommandTypeDto.ICE, text)
         }
     }
 
     private fun handleStateMessage(message: String) {
         val state = getSeparatedMessage(message)
-        val sessionState = WebRTCSessionState.typeOf(state)
+        val sessionState = WebRTCSessionTypeDto.typeOf(state)
         _sessionStateFlow.update { sessionState }
     }
 
-    private suspend fun handleSignalingCommand(command: SignalingCommand, text: String) {
+    private suspend fun handleSignalingCommand(command: SignalingCommandTypeDto, text: String) {
         val value = getSeparatedMessage(text)
-        _signalingCommandFlow.emit(command to value)
+        _signalingCommandTypeFlow.emit(command to value)
     }
 
     private fun getSeparatedMessage(text: String) = text.substringAfter(' ')
