2957b16efed5155c23e412db3982a1399d0d5e17
yeonjun <kgbduswns@gmail.com>
2024-04-25 17:00:00 +0900

    프로젝트 gradle alias 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
-    id("com.google.dagger.hilt.android") version "2.49" apply false
-    id("com.android.library") version "8.0.0" apply false
-    id("org.jetbrains.kotlin.jvm") version "1.8.10" apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.jetbrains.android) apply false
+    alias(libs.plugins.jetbrains.jvm) apply false
 }
 
 val clean by tasks.registering(Delete::class) {
