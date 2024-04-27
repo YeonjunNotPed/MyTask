02692d4a8905e32d82a8ffac2a38485d183128da
yeonjun <kgbduswns@gmail.com>
2024-04-27 15:00:00 +0900

    setting gradle, ui gradle에 domain 추가

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -67,6 +67,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":domain"))
     implementation(project(":data"))
     implementation(project(":core:common"))
     implementation(project(":core:model-ui"))
