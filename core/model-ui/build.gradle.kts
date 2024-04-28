7d1b663b5a5e04512badf799246c8a44e9d9e88d
yeonjun <kgbduswns@gmail.com>
2024-04-28 15:00:00 +0900

    ui-model 관련 의존성 추가

--- core/model-ui/build.gradle.kts
+++ core/model-ui/build.gradle.kts
@@ -1,8 +1,34 @@
 plugins {
-    alias(libs.plugins.jetbrains.jvm)
+    kotlin("kapt")
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.jetbrains.android)
+}
+
+android {
+    namespace = "com.youhajun.core.model.ui"
+    compileSdk = Configs.COMPILE_SDK
+
+    defaultConfig {
+        minSdk = Configs.MIN_SDK
+    }
+
+    kotlinOptions {
+        jvmTarget = Configs.JVM_TARGET
+    }
+
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.13"
+    }
 }
 
 dependencies {
     implementation(project(":core:model-data"))
     implementation(libs.kotlinx.collections.immutable)
+    implementation(libs.androidx.annotation.jvm)
+    implementation(libs.bundles.compose.ui)
+    implementation(libs.bundles.compose.libs)
+    implementation(libs.bundles.webrtc)
 }
