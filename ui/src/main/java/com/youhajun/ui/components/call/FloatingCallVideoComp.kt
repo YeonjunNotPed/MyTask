a22b138cbcbb988eeeb323d8b0b965587a64809b
yeonjun <kgbduswns@gmail.com>
2024-04-30 15:00:00 +0900

    FloatingCallVideoComp TrackVo, EglBaseContextWrapper 리컴포지션 최적화

--- ui/src/main/java/com/youhajun/ui/components/call/FloatingCallVideoComp.kt
+++ ui/src/main/java/com/youhajun/ui/components/call/FloatingCallVideoComp.kt
@@ -33,18 +33,18 @@ import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
-import com.youhajun.ui.models.holder.CallMediaStateHolder
+import com.youhajun.model_ui.holder.CallMediaStateHolder
 import com.youhajun.ui.R
-import org.webrtc.EglBase
+import com.youhajun.model_ui.wrapper.EglBaseContextWrapper
+import com.youhajun.model_ui.vo.webrtc.TrackVo
 import org.webrtc.RendererCommon
-import org.webrtc.VideoTrack
 
 @Composable
 fun FloatingCallVideoComp(
     modifier: Modifier = Modifier,
-    videoTrack: VideoTrack,
+    trackVo: TrackVo,
     mediaStateVo: CallMediaStateHolder,
-    eglBaseContext: EglBase.Context,
+    eglBaseContextWrapper: EglBaseContextWrapper,
     parentBounds: IntSize,
     paddingValues: PaddingValues = PaddingValues(0.dp, 0.dp)
 ) {
@@ -129,9 +129,9 @@ fun FloatingCallVideoComp(
 
         if (mediaStateVo.isCameraEnable) {
             MyTaskVideoRenderer(
-                videoTrack = videoTrack,
+                trackVo = trackVo,
                 modifier = Modifier.fillMaxSize(),
-                eglBaseContext = eglBaseContext,
+                eglBaseContextWrapper = eglBaseContextWrapper,
                 rendererEvents = rendererEvents,
                 isFrontCamera = mediaStateVo.isFrontCamera
             )
