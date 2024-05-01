c4f68e434cf46d42cd0588bfe71e7f16cae1f161
yeonjun <kgbduswns@gmail.com>
2024-05-01 17:00:00 +0900

    GptMessageFooterState list ->ImmutableList 리컴포지션 최적화

--- core/model-ui/src/main/java/com/youhajun/model_ui/types/gpt/GptMessageFooterState.kt
+++ core/model-ui/src/main/java/com/youhajun/model_ui/types/gpt/GptMessageFooterState.kt
@@ -1,8 +1,9 @@
 package com.youhajun.model_ui.types.gpt
 
 import com.youhajun.model_ui.vo.gpt.GptRecommendUrlVo
+import kotlinx.collections.immutable.ImmutableList
 
 sealed class GptMessageFooterState {
     data object ShowCreateAt: GptMessageFooterState()
-    data class ShowRecommendingUrl(val recommendUrlVoList: List<GptRecommendUrlVo>) : GptMessageFooterState()
+    data class ShowRecommendingUrl(val recommendUrlVoList: ImmutableList<GptRecommendUrlVo>) : GptMessageFooterState()
 }
