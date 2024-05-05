03cc11c8fce8ace0094d7826489767ac59c44c8f
yeonjun <kgbduswns@gmail.com>
2024-05-05 17:00:00 +0900

    MainViewModel 변경된 코드 적용

--- ui/src/main/java/com/youhajun/ui/viewModels/MainViewModel.kt
+++ ui/src/main/java/com/youhajun/ui/viewModels/MainViewModel.kt
@@ -1,19 +1,15 @@
 package com.youhajun.ui.viewModels
 
 import androidx.lifecycle.ViewModel
-import com.youhajun.domain.usecase.sign.PostLoginUseCase
-import com.youhajun.domain.usecase.sign.PostSocialLoginUseCase
-import com.youhajun.ui.models.sideEffects.LoginSideEffect
-import com.youhajun.ui.models.sideEffects.MainSideEffect
-import com.youhajun.ui.models.states.LoginState
-import com.youhajun.ui.models.states.MainState
+import com.youhajun.model_ui.sideEffects.MainSideEffect
+import com.youhajun.model_ui.states.MainState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import org.orbitmvi.orbit.Container
 import org.orbitmvi.orbit.ContainerHost
 import org.orbitmvi.orbit.viewmodel.container
 import javax.inject.Inject
 
-interface MainIntent {
+private interface MainIntent {
 
 }
 
